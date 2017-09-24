b79c6311ed984be7b7f339dcfefe2c9628179c28
atul rajsheel <rajsheel@gmail.com>
2017-09-24 09:31:34 -0400

    Optimized the code

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,22 +1,13 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
 
-	start <- end <- 0
 	pollutant_data_total <- numeric()
+	file_names <- list.files("specdata",full.names = TRUE)
+	
 	for (i in id) {
 
-		file_nam <- paste(directory, "/", formatC(i, width = 3, flag = 0), ".csv", sep = "")
+		file_pollutant_data <- read.csv(file_names[i])
+		pollutant_data_total <- c(pollutant_data_total, file_pollutant_data[,pollutant])
 		
-		pollutant_data <- read.csv(file_nam)
-		
-		pollutant_data_type <- pollutant_data[, pollutant]
-		
-		size <- length(pollutant_data_type)
-		
-		if (size > 0) {
-			start <- end + 1
-			end <- start + size - 1
-			pollutant_data_total[start:end] <- pollutant_data_type
-		}
 	}
-	mean(pollutant_data_total[!is.na(pollutant_data_total)])
+	mean(pollutant_data_total,na.rm = TRUE)
 }
